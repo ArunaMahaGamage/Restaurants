3d46c53ec2419adfe94a944d3abc3340df13bf30
ArunaMahaGamage <admgbit@gmail.com>
2018-02-15 13:59:52 +0530

    added Grid View

--- app/src/main/java/com/witellsoluations/resturants/resturants/MainActivity.java
+++ app/src/main/java/com/witellsoluations/resturants/resturants/MainActivity.java
@@ -1,13 +1,38 @@
 package com.witellsoluations.resturants.resturants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.witellsoluations.resturants.resturants.user.User;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_sigin_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+
+        btn_sigin_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                siginIn();
+            }
+        });
+    }
+
+    void  initUI() {
+        btn_sigin_in = findViewById(R.id.btn_sigin_in);
+    }
+
+    void siginIn() {
+
+        Intent userIntent = new Intent(this,User.class);
+        startActivity(userIntent);
     }
 }
--- app/src/main/java/com/witellsoluations/resturants/resturants/user/User.java
+++ app/src/main/java/com/witellsoluations/resturants/resturants/user/User.java
@@ -2,14 +2,50 @@ package com.witellsoluations.resturants.resturants.user;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.GridView;
 
 import com.witellsoluations.resturants.resturants.R;
 
 public class User extends AppCompatActivity {
 
+    RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+
+    GridView gridview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        initUI();
+
     }
+
+    void initUI() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        gridview = (GridView) findViewById(R.id.gridview);
+        recyclerView();
+    }
+
+    void recyclerView() {
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"A","B","C","D","E","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+
+        gridview.setAdapter(new ImageAdapter(this));
+    }
+
 }
