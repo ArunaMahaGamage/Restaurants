79c1c540dddea6cca063a805fc868858e746c1f0
ArunaMahaGamage <admgbit@gmail.com>
2018-02-15 11:06:30 +0530

    first commit

---
+++ app/src/main/java/com/witellsoluations/resturants/resturants/user/User.java
@@ -0,0 +1,15 @@
+package com.witellsoluations.resturants.resturants.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.witellsoluations.resturants.resturants.R;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
